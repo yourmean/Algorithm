1d1d9cc3048d4b79fa70cf188e6b052b1675f5fa
Yumin <55529646+yourmean@users.noreply.github.com>
2021-01-19 22:42:33 +0900

    210118

--- BOJ_2884_2.py
+++ BOJ_2884_2.py
@@ -1,12 +1,9 @@
 #BOJ_2884
 #https://www.acmicpc.net/problem/2884
 
-# sol1) 성실하게 if문 써서 풀기
+# sol2)
 h,m = map(int, input().split(' '))
-if m>=45:
-    print(h,m-45)
-else:
-    if h>0:
-        print(h-1, m+15)
-    else:
-        print(23, m+15)
+
+# m<45면 h-1, m>=45면 h 그대로 -> 시
+# (m-45)%60 -> 분
+print((h-(m<45))%24, (m-45)%60)
--- BOJ_9498_2.py
+++ BOJ_9498_2.py
@@ -1,5 +1,15 @@
 #BOJ_9498
 #https://www.acmicpc.net/problem/9498
 
-# 0, 10, 20, ... 100 기준 -> 10으로 나눈 몫에 따라 학점 부여
-print("FFFFFFDCBAA"[int(input())//10])
+# sol1) 성실하게 if문 써서 풀기
+n = int(input())
+if 89<n<=100:
+    print('A')
+elif 79<n<90:
+    print('B')
+elif 69<n<90:
+    print('C')
+elif 59<n<90:
+    print('D')
+else:
+    print('F')
